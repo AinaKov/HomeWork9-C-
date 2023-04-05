43b41e2d969c1dc223a909815aada25962fb2555
AlinaKov <alkovkova@gmail.com>
2023-04-06 02:35:02 +0500

    Added HW9

---
+++ Task64/Task64.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.Clear(); 
+Console.WriteLine("Введите первое число"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите второе число"); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine(PrintNumbers(a, b)); 
+
+
+string PrintNumbers(int a, int b) 
+{ 
+    if (a == b) 
+    { 
+        return a.ToString();
+
+    } 
+    if (a > b)
+    {
+      return (a + " " + PrintNumbers(a - 1, b)); 
+    }
+
+    return (a + " " + PrintNumbers(a + 1, b)); 
+}
---
+++ Task66/Task66.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear(); 
+Console.WriteLine("Введите первое число"); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите второе число"); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+
+
+Console.WriteLine(Sum(a, b));
+int Sum(int m, int n)
+{
+    if (n == m) return n;
+    return m + Sum(m + 1, n);
+}
